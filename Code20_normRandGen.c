ec53ff595cbd666bbdabea5e176887ec116a7dbf
t-shrot <tammash@sce.ac.il>
2026-04-13 12:06:34 +0300

    Create Code20_normRandGen.c

---
+++ Code20_normRandGen.c
@@ -0,0 +1,8 @@
+float a = (float)rand() / (float)RAND_MAX;
+float b = (float)rand() / (float)RAND_MAX;
+float c = (float)rand() / (float)RAND_MAX;
+
+char str_a[20], str_b[20], str_c[20];
+sprintf(str_a, "%f", a);
+sprintf(str_b, "%f", b);
+sprintf(str_c, "%f", c);
